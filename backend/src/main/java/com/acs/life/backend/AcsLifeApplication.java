46bb16d1efd90738bdf1cde811eb2bf9320e2b7e
David Capragiu <davidcapragiu@Davids-MacBook-Air.local>
2023-03-07 13:30:47 +0200

    backend skel

---
+++ backend/src/main/java/com/acs/life/backend/AcsLifeApplication.java
@@ -0,0 +1,13 @@
+package com.acs.life.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsLifeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acs/life/backend/AcsLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acs.life.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcsLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
