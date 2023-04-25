82ae65dfceb1fe027175445e4c27cfea12bcf102
David Capragiu <deividcapragiu@gmail.com>
2023-04-25 08:15:18 +0300

    admin panel context logic and page switchers

---
+++ frontend/src/components/StudentsView.tsx
@@ -0,0 +1,13 @@
+import {
+    VStack,
+    Text
+} from "@chakra-ui/react";
+
+export const StudentsView: React.FC = () => {
+    return (
+        // @ts-ignore
+        <VStack>
+            <Text>Students page</Text>
+        </VStack>
+    );
+}
