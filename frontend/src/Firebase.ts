aff8c39eb9f20eab41b987e124b05bccfb089c34
Dievaid <davidcapragiu.esports@gmail.com>
2023-05-10 05:05:00 +0300

    Conjoined database

--- frontend/src/Firebase.ts
+++ frontend/src/Firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbnocq_T_NNQw1thPeimoyrKDM82hK1T8",
-  authDomain: "proiect-ip-c3429.firebaseapp.com",
-  projectId: "proiect-ip-c3429",
-  storageBucket: "proiect-ip-c3429.appspot.com",
-  messagingSenderId: "251133083444",
-  appId: "1:251133083444:web:ea2ee6e7bb65ac7e686f61",
-  measurementId: "G-ZQVTJMLPQS"
+  apiKey: "AIzaSyC3tvx90oBKRHDZMbdlKaJCKGVK5HYuzW0",
+  authDomain: "acs-life-703d6.firebaseapp.com",
+  projectId: "acs-life-703d6",
+  storageBucket: "acs-life-703d6.appspot.com",
+  messagingSenderId: "1088572334830",
+  appId: "1:1088572334830:web:47b388e92560818012c9ff",
+  measurementId: "G-FDBWKG6119"
 };
 
 const fb = initializeApp(firebaseConfig);
