0c989921a1a68e319e23c6c468d18830fcbc723c
Dievaid <davidcapragiu.esports@gmail.com>
2023-05-29 05:57:03 +0300

    Email server with otp encryption and postgres

--- email-server/index.ts
+++ email-server/index.ts
@@ -2,6 +2,37 @@ import express = require('express');
 import bodyParser = require('body-parser');
 import cors = require('cors');
 import nodemailer = require('nodemailer');
+import bcrypt = require('bcrypt');
+
+require('dotenv').config();
+
+const { Client } = require('pg');
+
+const clientData = {
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.db,
+    password: process.env.pwd,
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+};
+
+const client = new Client(clientData);
+
+client.connect((err: any) => {
+    if (err) throw err;
+    const createTableSql = `
+        CREATE TABLE IF NOT EXISTS AUTH (
+            email varchar(255) PRIMARY KEY,
+            otp varchar(255)
+        )
+    `;
+    const deleteTableSql = 'DELETE FROM AUTH';
+    client.query(createTableSql).then(() =>
+        client.query(deleteTableSql).then(() => console.log("Connected and init done!")))
+});
 
 const app: express.Application = express();
 
@@ -23,30 +54,33 @@ const transporter = nodemailer.createTransport({
     secure: true
 });
 
-const otpMap = new Map<string, string>();
-
-app.post("/get-otp", (req, res) => {
+app.post("/get-otp", async (req, res) => {
     const email: string = req.body.email;
-    
-    otpMap.set(
-        email,
-        Math.random().toString(36).slice(-8)
-    );
+    const otp = Math.random().toString(36).slice(-8);
 
-    setTimeout(() => {
-        //! Invalidate otp after 5 minutes
-        otpMap.delete(email);
-    }, 300000);
+    const deleteIfHashExistsAlready = `DELETE FROM AUTH WHERE email = $1`;
+    await client.query(deleteIfHashExistsAlready, [email]);
+    
+    bcrypt.hash(otp, 10, (err, hash) => {
+        if (err) return;
+        const insertSql = 'INSERT INTO AUTH (email, otp) values ($1, $2)';
+        client.query(insertSql, [email, hash]).then(() => {
+            setTimeout(async () => {
+                //! Invalidate otp after 5 minutes
+                const deleteSql = 'DELETE FROM AUTH WHERE email = $1';
+                await client.query(deleteSql, [email]);
+            }, 300000);
+        });
+    });
 
     const mailData = {
         from: 'acslife.ip@gmail.com',
         to: email,
         subject: "Cod autentificare ACS Life",
-        html: `<p>Codul tău de autentificare este: ${otpMap.get(email)}</p>`
+        html: `<p>Codul tău de autentificare este: ${otp}</p>`
     }
     transporter.sendMail(mailData, (error, _) => {
         if (error) {
-            otpMap.delete(email);
             console.log(error);
             res.status(501).send({status: "failed"});
             return;
@@ -58,14 +92,26 @@ app.post("/get-otp", (req, res) => {
 app.post("/verify-otp", (req, res) => {
     const { otp, email } = req.body;
 
-    const actualOtp = otpMap.get(email);
+    const getOtpSql = 'select otp from auth where email = $1';
+    client.query(getOtpSql, [email])
+    .then((data: any) => {
+        try {
+            const actualOtp = data.rows[0].otp;
+            bcrypt.compare(otp, actualOtp, (err, same) => {
+                if (err) throw err;
 
-    if (actualOtp && otp === actualOtp) {
-        otpMap.delete(email);
-        res.send({status: "success"});
-    } else {
-        res.status(501).send({status: "failed"});
-    }
+                if (same) {
+                    const deleteOtp = `DELETE FROM AUTH WHERE email = $1`;
+                    client.query(deleteOtp, [email]).then(() => res.send({status: "success"}));
+                } else {
+                    res.status(400).send({status: "failed"});
+                }
+            })
+        }
+        catch {
+            res.status(400).send({status: "failed"});
+        }
+    });
 });
 
 const port = process.env.PORT || 4000 
